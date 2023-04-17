cf2ba3adab7f3c8696622d4523915c6e0497bb9f
Athan Reines <kgryte@gmail.com>
2023-04-16 20:26:22 -0700

    Update copyright

--- scripts/build_index.js
+++ scripts/build_index.js
@@ -3,7 +3,7 @@
 /**
 * @license MIT
 *
-* Copyright (c) 2023 Python Data APIs Consortium.
+* Copyright (c) 2023 Consortium for Python Data API Standards.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
--- scripts/css/icons.css
+++ scripts/css/icons.css
@@ -1,7 +1,7 @@
 /**
 * @license MIT
 *
-* Copyright (c) 2023 Python Data APIs Consortium.
+* Copyright (c) 2023 Consortium for Python Data API Standards.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
